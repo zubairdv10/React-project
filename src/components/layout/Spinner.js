e27fbc3122d770dc269c9a67dc8208d19a072b66
zubairdv10 <zubairdevries127@gmail.com>
2020-06-12 21:34:05 +0200

    message

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import giphy from "./giphy.gif";
+
+const Spinner = () => (
+  <Fragment>
+    <img
+      src={giphy}
+      alt="Loading..."
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
+  </Fragment>
+);
+
+export default Spinner;
